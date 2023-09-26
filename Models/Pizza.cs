10b0f619e3ab6b447e16b2600bc001c7f0371131
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-09-26 16:10:21 -0600

    updated model and removed price image

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -9,7 +9,5 @@ public class Pizza
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
-    public decimal Price { get; set; }
-    public string? ImageUrl { get; set; }
     public bool IsGlutenFree { get; set; }
 }
