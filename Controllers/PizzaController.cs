a47544d4c06c11c383a850c4828ba6547ac2be73
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-09-26 11:46:47 -0600

    Finished the PizzaController Logic

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ContosoPizza.Controllers;
 
+// the [ApiController] attribute indicates the paramerter is coming from the request body
 [ApiController]
 [Route("[controller]")]
 public class PizzaController : ControllerBase
@@ -29,15 +30,41 @@ public class PizzaController : ControllerBase
     // POST action
     // This code will save the pizza and return a result of 200-OK if successful.
     [HttpPost]
-    public IActionResult Create(Pizza pizza) { }
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+    }
 
     // PUT action
     // This code will update the pizza and return a result of 200-OK if successful.
     [HttpPut("{id}")]
-    public IActionResult Update(int id, Pizza pizza) { }
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        if (id != pizza.Id)
+            return BadRequest();
+
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza is null)
+            return NotFound();
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
+    }
 
     // DELETE action
     // This code will delete the pizza and return a result of 200-OK if successful.
     [HttpDelete("{id}")]
-    public IActionResult Delete(int id) { }
+    public IActionResult Delete(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza is null)
+            return NotFound();
+
+        PizzaService.Delete(id);
+
+        return NoContent();
+    }
 }
