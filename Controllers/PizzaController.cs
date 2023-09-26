16ed4ba5b666507e0a68e9e7efbe054722e4f4c1
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-09-26 11:20:36 -0600

    added CRUD fucntions to PizzaController

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -11,12 +11,33 @@ public class PizzaController : ControllerBase
     public PizzaController() { }
 
     // GET all action
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
 
     // GET by Id action
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza == null)
+            return NotFound();
+
+        return pizza;
+    }
 
     // POST action
+    // This code will save the pizza and return a result of 200-OK if successful.
+    [HttpPost]
+    public IActionResult Create(Pizza pizza) { }
 
     // PUT action
+    // This code will update the pizza and return a result of 200-OK if successful.
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza) { }
 
     // DELETE action
+    // This code will delete the pizza and return a result of 200-OK if successful.
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id) { }
 }
