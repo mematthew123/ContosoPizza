1b74a8d3fcac264bb97cc43d6a323e3fc154d0ca
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-09-26 11:08:23 -0600

    ran build command and created PizzaController

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,23 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaControler : ContollerBase
+{
+    public PizzaController()
+    {
+        // GET all action
+
+        // GET by Id action
+
+        // POST action
+
+        // PUT action
+
+        // DELETE action
+    }
+}
