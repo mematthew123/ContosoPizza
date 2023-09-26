17b703f97c35d4eb1baebe9f8cb6a7bbebd83df8
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-09-26 13:54:18 -0600

    added fields to Pizza Model

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -8,5 +8,8 @@ public class Pizza
 {
     public int Id { get; set; }
     public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public string? ImageUrl { get; set; }
     public bool IsGlutenFree { get; set; }
 }
